b7e155011a2ad11590b8bc6e05a9faf0b2510fd9
gamepro6390 <gamepro6390@gmail.com>
2022-08-05 10:49:43 +0900

    Chapter 05

--- C_Edu_05/C_Edu_05/소스.cpp
+++ C_Edu_05/C_Edu_05/소스.cpp
@@ -11,8 +11,10 @@ int main(void)
 {
 	int Number;
 	char* Name;
+
 	printf("입력 : ");
 	scanf_s("%d", &Number);
+
 	if (Number == 1)
 	{
 		Name = (char*)"hong";
@@ -21,11 +23,24 @@ int main(void)
 	{
 		Name = (char*)"Lim";
 	}
-	else {
+	else 
+	{
 		Name = (char*)"Lee";
 	}
 	printf("결과값 : %s\n", Name);
 
-
-
+	switch (Number)
+	{
+	case 1:
+		printf("홍길동");
+		break;
+	case 2:
+		printf("임꺽정");
+		break;
+	case 3:
+		printf("이몽룡");
+		break;
+	default:
+		printf("잘못입력하였습니다.");
+	}
 }

6cb45430f1c11918060c97454e55733640f012eb
gamepro6390 <gamepro6390@gmail.com>
2022-08-08 10:38:34 +0900

    Chapter 06

--- C_Edu_06/C_Edu_06/소스.cpp
+++ C_Edu_06/C_Edu_06/소스.cpp
@@ -27,5 +27,55 @@ int main(void)
 		printf("반복문 %d \n", i + 1);
 	}
 
+	do
+	{
+		//최초에 한번은 실행됨 
+		printf("최초에 한번은 실행.");
+		break;
+		//그후에 조건을 확인하고 조건이 true인 경우 반복
+	} while (false);
+	printf("\n");
+
+	//홀수 * 짝수 구구단
+	/*int x, y;
+	printf_s("시작단/끝단 입력 :");
+	scanf_s("%d %d",x,y);*/
+
+	for (int i = 2; i < 10; ++i)
+	{
+		if ((i % 2 == 0))
+			continue;
+		for (int j = 1; j < 10; ++j)
+		{
+			if ((j % 2 == 1))
+				continue;
+			printf("%d * %d = %d\n", i, j, i * j);
+		}
+		printf("\n");
+	}
+
+	int h, w;
+	for (h = 0; h < 3; h++)
+	{
+		for (w = 0; w< 5; w++)
+			printf("*");
+		printf("\n");
+	}
+	/*int a, b;
+	for (a = 0; a < 4; a++)
+	{
+		for (b = 0; b<4; b++)
+		{
+
+			if (a != b)
+				continue;
+			else if (a + b != 4)
+				continue;
+			printf("*");
+		}
+		printf("\n");
+	}*/
+
+
 	return 0;
 }

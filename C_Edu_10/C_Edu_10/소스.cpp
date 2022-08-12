c44b6728e40987703f635d8ba8dd48eb24680715
gamepro6390 <gamepro6390@gmail.com>
2022-08-12 10:19:12 +0900

    Chatper 10

--- C_Edu_10/C_Edu_10/소스.cpp
+++ C_Edu_10/C_Edu_10/소스.cpp
@@ -9,6 +9,7 @@ int main(void)
 {
 	//배열과 포인터의 관계
 	{
+		//배열 선언 및 초기화
 		int Array[8] = { 0 };
 		for (int i = 0; i < 8; ++i)
 		{
@@ -20,16 +21,16 @@ int main(void)
 		printf("\n");
 
 		printf("포인터를 사용한 출력\n");
-		
-		//배열은 인덱스(대괄호)를 뺀 상태로 주소반환연산자 없이 주소를 전달할수 있다.
+
+		//배열은 인덱스(대괄호)를 뺀 상태로 주소반환연산자 없이 주소를 전달할수 있다. &[]
 		int* iter = Array;
 
 		for (int i = 0; i < 8; ++i)
-		
-			printf("%d\n", (*(iter+i)) );
-		
+
+			printf("%d\n", (*(iter + i)));
+
 		printf("\n");
-	
+
 	}
 	{
 		//문제// 
@@ -44,6 +45,44 @@ int main(void)
 		printf("\n");
 
 	}
+
+	{
+		//로또 번호를 생성하는 프로그램 작성
+		int Number[5][6];
+		for (int i = 0; i < 5; ++i)
+			for (int j = 0; j < 6; ++j)
+			{
+				srand((i + 1) * (j + 1) * rand());
+				Number[i][j] = rand() % 45 + 1;
+			}
+		for (int i = 0; i < 5; ++i)
+		{
+			printf("%02d. ", i + 1);
+			for (int j = 0; j < 6; ++j)
+			{
+				printf("%02d  ", Number[i][j]);
+			}
+			printf("\n");
+		}
+
+	}
+	//버블 정렬
+	/*int Number[5][6];
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j < 5; ++j)
+		{
+			for (int k = j + 1; k < 6; ++k)
+			{
+				if (Number[i][j] > Number[i][k])
+				{
+					int Temp = Number[i][j];
+					Number[i][j] = Number[i][k];
+					Number[i][k] = Temp;
+				}
+			}
+		}
+	}*/
 
 	return 0;
 }

f4b40c8b23e22e1462af3808b73490da058b9338
gamepro6390 <gamepro6390@gmail.com>
2022-08-18 11:22:07 +0900

    Chapter 13

--- C_Edu_13/C_Edu_13/Main.cpp
+++ C_Edu_13/C_Edu_13/Main.cpp
@@ -19,13 +19,29 @@ struct Student
 	int Math;
 };
 
-void Output(Student* _std);
+void Output(Student* _std, int _size);
 
 
 
 int main(void)
 {
-	Student Std;
+	Student Std[10];
+
+	
+	Std[0].Name = (char*)"홍길동";
+	//배열의 크기를 알 수 있다.
+	int Count = sizeof(Std) / sizeof(Std[0]);
+
+	for (int i = 0; i < sizeof(Std)/sizeof(Std[0]); ++i)
+	{
+		Std[i].Kor = i * 10;
+		Std[i].Eng = i * 20;
+		Std[i].Math = i * 30;
+		Std[i].Number = i * 1;
+
+	}
+
+	/*Student Std;
 	Std.Name = (char*)"홍길동";
 	Std.Kor = 100;
 	Std.Eng = 90;
@@ -38,19 +54,24 @@ int main(void)
 	printf("영어 점수: %d\n", Std.Eng);
 	printf("수학 점수: %d\n", Std.Math);
 	printf("번호: %d\n\n", Std.Number);
+	*/
 
-	Output(&Std);
 
+	Output(Std, sizeof(Std)/sizeof(Std[0]));
 
 	return 0;
 }
 
-
-void Output(Student* _std)
+//매개변수로 배열이 전달 될때에는 배열의 크기를 함께 전달 해주어야 한다.
+void Output(Student* _std, int _size)
 {
-	printf("학생 이름:%s\n", _std->Name);
-	printf("국어 점수:%d\n", _std->Kor);
-	printf("영어 점수:%d\n", _std->Eng);
-	printf("수학 점수:%d\n", _std->Math);
-	printf("번호:%d\n", _std->Number);
+	for (int i = 0; i < _size; ++i)
+	{
+		//printf("학생 이름:%s\n", (_std + i)->Name);
+		printf("국어 점수:%d\n", (_std + i)->Kor);
+		printf("영어 점수:%d\n", (_std + i)->Eng);
+		printf("수학 점수:%d\n", (_std + i)->Math);
+		printf("학생 번호:%d\n", (_std + i)->Number);
+		printf("\n");
+	}
 }

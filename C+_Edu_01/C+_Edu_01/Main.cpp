ae3357ce7471effa56773f220433889f8a67025c
gamepro6390 <gamepro6390@gmail.com>
2022-08-24 11:25:27 +0900

    C+_Edu_01

---
+++ C+_Edu_01/C+_Edu_01/Main.cpp
@@ -0,0 +1,87 @@
+/*
+Chapter 01
+*/
+//기본 입출력
+#include <stdio.h>
+
+//system 함수
+#include <stdlib.h>
+
+//input 및 윈도우창 제어 할때 사용될 라이브러리
+#include <Windows.h>
+
+
+/*
+struct Student
+{
+	char* Name;
+	int Kor;
+	int Eng;
+	int Math;
+};
+
+typedef struct Object
+{
+	Student std;
+	int Age;
+	int Number;
+	int iClass;
+}HOBJ;  //*PHOBJ;
+//핸들은 포인터로 두진 않음.
+*/
+struct Time
+{
+	int Hour;
+	int Minute;
+	int Second;
+};
+
+int main(void)
+{
+	//루프
+	//HOBJ
+	//*PHOBJ
+	
+	
+	//시간 함수
+	// DWORD Time =GetTickCount(); 컴퓨터가 켜진 순간부텉 49.7일
+
+	//GetTickcount64()=1/1000초 =0.001초 단위
+	ULONGLONG ITime = GetTickCount64();
+	int Count = 0;
+	Time tTime;
+	tTime.Hour =23;
+	tTime.Minute = 58;
+	tTime.Second = 0;
+	while (true)
+	{
+		//if 특정 이벤트가 발생 한다면 ...
+		//else 아니라면...
+		if (ITime+50< GetTickCount64())
+		{
+			//루프
+			ITime = GetTickCount64();
+			//화면을 지워준다.
+			system("cls");
+			//tTime.Second=tTime.Second + ITime;
+			printf("%02d:%02d:%02d\n",tTime.Hour,tTime.Minute,++tTime.Second);
+
+			if (tTime.Second >= 60)
+			{
+				tTime.Second = 0;
+				++tTime.Minute;
+			}
+			if (tTime.Minute >= 60)
+			{
+				tTime.Minute = 0;
+				++tTime.Hour;
+			}
+			if (tTime.Hour >= 24)
+			{
+				tTime.Hour = 0;
+				
+			}
+		}
+	}
+	return 0;
+}
